b9178ea3a702250029214b874f38ed1b6f33af70
Daniel Chmielewski <danielchm6@gmail.com>
2025-06-05 14:16:20 -0400

    Write logic to play entire game

--- game.js
+++ game.js
@@ -18,32 +18,48 @@ function getHumanChoice() {
   return userInput;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  let currRound = 1;
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toUpperCase();
-  computerChoice = computerChoice.toUpperCase();
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
 
-  // console.log(humanChoice);
-  // console.log(computerChoice);
-  if (humanChoice === computerChoice) {
-    console.log("Draw!");
-  } else if (
-    (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
-    (humanChoice === "SCISSORS" && computerChoice === "PAPER") ||
-    (humanChoice === "PAPER" && computerChoice === "ROCK")
-  ) {
-    console.log("You win! " + humanChoice + " beats " + computerChoice);
-    humanScore++;
-  } else {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    computerScore++;
+    // console.log(humanChoice);
+    // console.log(computerChoice);
+    if (humanChoice === computerChoice) {
+      console.log("Draw!");
+    } else if (
+      (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+      (humanChoice === "SCISSORS" && computerChoice === "PAPER") ||
+      (humanChoice === "PAPER" && computerChoice === "ROCK")
+    ) {
+      console.log("You win! " + humanChoice + " beats " + computerChoice);
+      humanScore++;
+    } else {
+      console.log("You lose! " + computerChoice + " beats " + humanChoice);
+      computerScore++;
+    }
+    //   console.log(humanScore);
+    //   console.log(computerScore);
   }
-  //   console.log(humanScore);
-  //   console.log(computerScore);
+
+  while (humanScore < 5 && computerScore < 5) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log("-----Round " + currRound + "-----");
+    playRound(humanSelection, computerSelection);
+    currRound++;
+    console.log(humanScore);
+    console.log(computerScore);
+  }
+  if (humanScore === 5) {
+    console.log("You won the game! " + humanScore + "-" + computerScore);
+  } else if (computerScore === 5) {
+    console.log("You lost the game! " + humanScore + "-" + computerScore);
+  }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+playGame();
