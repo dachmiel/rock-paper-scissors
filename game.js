1f79d86fa33f760f04ce4f19df206983c281927a
Daniel Chmielewski <danielchm6@gmail.com>
2025-06-05 14:12:39 -0400

    Write logic to get computer choice

---
+++ game.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let result;
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    result = "Rock";
+  } else if (randomNumber === 2) {
+    result = "Paper";
+  } else if (randomNumber === 3) {
+    result = "Scissors";
+  }
+  //   console.log(result);
+  return result;
+}
