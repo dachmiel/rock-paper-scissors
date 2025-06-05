38ae1a2649a1045aa15ee336bfa092b0c28bf3ac
Daniel Chmielewski <danielchm6@gmail.com>
2025-06-05 14:15:43 -0400

    Write logic to play single round

--- game.js
+++ game.js
@@ -17,3 +17,33 @@ function getHumanChoice() {
   //   console.log(userInput);
   return userInput;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
+
+  // console.log(humanChoice);
+  // console.log(computerChoice);
+  if (humanChoice === computerChoice) {
+    console.log("Draw!");
+  } else if (
+    (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (humanChoice === "SCISSORS" && computerChoice === "PAPER") ||
+    (humanChoice === "PAPER" && computerChoice === "ROCK")
+  ) {
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+    humanScore++;
+  } else {
+    console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    computerScore++;
+  }
+  //   console.log(humanScore);
+  //   console.log(computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
